fa314823160256c9dbb384fc22d7802d88f1331c
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2026-02-20 11:58:36 +0530

    Implement Fibonacci function in fib.cpp

---
+++ fib.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fib(int n){
+    return n<2?n:fib(n-1)+fib(n-2);
+}
+int main() {
+    int n;
+    cin>>n;
+    cout<<fib(n);
+
+    return 0;
+}
